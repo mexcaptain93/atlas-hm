1b00a1a05aa28fc4b0d09ece43f6fd9cf0cda483
Mihail <mexcaptain@gmail.com>
2018-07-13 15:30:41 +0300

    Work on index page

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    svg4everybody({});
+    indexSlider();
 });
+
+function indexSlider() {
+    var indexSlider = $('.js-index-slider');
+
+    if (indexSlider.length > 0) {
+        indexSlider.slick({
+
+        });
+
+        $('.js-index-slider-prev').on('click', function (e) {
+            e.preventDefault();
+            indexSlider.find('.slick-prev').click();
+        });
+
+        $('.js-index-slider-next').on('click', function (e) {
+            e.preventDefault();
+            indexSlider.find('.slick-next').click();
+        });
+    }
+
+
+}
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -6,21 +6,21 @@ module.exports = function() {
             .pipe($.browserSync.reload({
                 stream: true
             }));
-    });
+});
 
     $.gulp.task('libsJS:build', () => {
         return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gp.uglifyjs())
             .pipe($.gulp.dest('./build/static/js/'));
-    });
+});
 
     $.gulp.task('js:copy', () => {
         return $.gulp.src(['./dev/static/js/*.js',
-                           '!./dev/static/js/libs.min.js'])
+            '!./dev/static/js/libs.min.js'])
             .pipe($.gulp.dest('./build/static/js/'))
             .pipe($.browserSync.reload({
                 stream: true
             }));
-    });
+});
 };
